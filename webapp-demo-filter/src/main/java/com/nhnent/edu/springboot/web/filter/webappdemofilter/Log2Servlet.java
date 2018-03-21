9eebad8fc797ed4ac580b77ec058a1b915fc6d3e
정지범 <zbum@naver.com>
2018-03-21 19:35:27 +0900

    add servlet example

---
+++ webapp-demo-filter/src/main/java/com/nhnent/edu/springboot/web/filter/webappdemofilter/Log2Servlet.java
@@ -0,0 +1,15 @@
+package com.nhnent.edu.springboot.web.filter.webappdemofilter;
+
+import org.springframework.web.servlet.HttpServletBean;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Log2Servlet extends HttpServletBean{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Log2Servlet!!");
+    }
+}
