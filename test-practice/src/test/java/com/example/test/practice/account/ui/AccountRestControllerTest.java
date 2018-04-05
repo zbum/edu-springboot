0f5b541903f0c32a9d9263759496dfc8e3318a1f
myeongju.jung <myeongju.jung@nhnent.com>
2018-04-05 14:54:13 +0900

    Test

--- test-practice/src/test/java/com/example/test/practice/account/ui/AccountRestControllerTest.java
+++ test-practice/src/test/java/com/example/test/practice/account/ui/AccountRestControllerTest.java
@@ -14,7 +14,6 @@ import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * WebMvcTest와 MockBean을 이용한 웹 부분은 통합으로 로딩하고 나머지는 단위적으로 테스트
@@ -39,12 +38,12 @@ public class AccountRestControllerTest {
         given(accountService.getAccount(accountId)).willReturn(account);
         // when
         mockMvc.perform(get("/api/accounts/{0}", accountId)
-                                .accept(MediaType.APPLICATION_JSON))
+                            .accept(MediaType.APPLICATION_JSON))
                // then
-               .andExpect(status().isOk()) // FIXME 1-3 : 성공 응답
+               //.andExpect(status().is__()) // FIXME 1-3 : 성공 응답
                .andExpect(jsonPath("$.accountId").value(account.getAccountId()))
                //.andExpect(jsonPath("$.____").value(account.getName()))  // FIXME 1-4 : 성공 응답
-               ;
+        ;
         // then2
         then(accountService).should(times(2)).getAccount(accountId);    // ?!
     }
