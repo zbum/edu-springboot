4712449233860ff1197caa0b407830eb52ddb1ce
myeongju.jung <myeongju.jung@nhnent.com>
2018-04-05 14:56:28 +0900

    Multi datasource sample

---
+++ multi-datasource-sample/src/main/java/com/example/multi/datasource/sample/MultiDatasourceSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.multi.datasource.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDatasourceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDatasourceSampleApplication.class, args);
+	}
+}
