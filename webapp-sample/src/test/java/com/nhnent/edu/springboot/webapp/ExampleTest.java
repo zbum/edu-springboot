f1d40d6721cfdc7728bafda4f43be83a183b5363
정지범 <zbum@naver.com>
2018-04-08 18:20:50 +0900

    add security example with database

---
+++ webapp-sample/src/test/java/com/nhnent/edu/springboot/webapp/ExampleTest.java
@@ -0,0 +1,16 @@
+package com.nhnent.edu.springboot.webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleTest {
+
+    @Test
+    public void loadContext() {
+    }
+
+}
